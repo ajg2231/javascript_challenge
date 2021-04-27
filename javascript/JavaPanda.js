9295054eb7f890437d9c28a183548a894d7f53c7
ajg2231 <43033837+ajg2231@users.noreply.github.com>
2021-04-27 07:07:33 -0400

    Update JavaPanda.js

--- javascript/JavaPanda.js
+++ javascript/JavaPanda.js
@@ -1,33 +1,56 @@
-var a = d3.select("#ufo-table");
 
-data.forEach((UFO) => {
-    var b = tbody.append("tr");
-    Object.entries(UFO).forEach(([key, value]) => {
-        var c = a.append("td");
-        c.text(value);
-    });
-});
+// from data.js
+var tableData = data;
 
-var d = d3.select("#filter-btn");
-
-var e = d3.select("#form");
+var tbody = d3.select("#ufo-table");
 
 
-button.on("click", hopeForTheBest);
-form.on("submit", hopeForTheBest);
+//fill table
+fillTable(data);
 
-function hopeForTheBest() {
-    
-    d3.event.preventDefault();
-    
-    var f = d3.select("#datetime");
-
-    var g = f.property("value");
-
-    console.log(g);
-    console.log(data);
-
-    var h = data.filter(dates => dates.datetime === g);
-
-    console.log(h);
+//append table
+function fillTable(data) {
+    tbody.html("");
+    data.forEach((UFOsights) => {
+        var row = tbody.append("tr");
+        Object.entries(UFOsights).forEach(([key, value]) => {
+            var cell = tbody.append("td");
+            cell.text(value);
+        });
+    })
 }
+
+// create event handler and filter for input
+// select button
+var button = d3.select("#filter-btn");
+
+//select form
+var form = d3.select("#form");
+
+//create event handlers
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+// complete event handler function for the form
+function runEnter() {
+
+    // prevent the page from refreshing
+    d3.event.preventDefault();
+
+ // select the input element and get the raw html node // select the value property of the input element
+ var datetime_input = d3.select("#datetime").property("value");
+
+ 
+ // log input date
+ console.log(datetime_input);
+ 
+ //filtered data
+ if (datetime_input) {
+ var filteredData = data.filter(dates => dates.datetime === datetime_input); }
+
+    //log filtered values
+    console.log(filteredData);
+
+    //filtered data tables
+    fillTable(filteredData);
+};
