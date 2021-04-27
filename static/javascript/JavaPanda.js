2809d1df660e91b3f4c5e87a26ae5b2630548dee
ajg2231 <43033837+ajg2231@users.noreply.github.com>
2021-04-27 07:39:40 -0400

    Update JavaPanda.js

--- static/javascript/JavaPanda.js
+++ static/javascript/JavaPanda.js
@@ -2,9 +2,7 @@
 // from data.js
 var tableData = data;
 
-var tbody = d3.select("#ufo-table");
-
-
+var v = d3.select("#ufo-table");
 
 tabble(data);
 
@@ -12,44 +10,38 @@ tabble(data);
 function tabble(data) {
     tbody.html("");
     data.forEach((UFOsights) => {
-        var row = tbody.append("tr");
+        var row = v.append("tr");
         Object.entries(UFOsights).forEach(([key, value]) => {
-            var cell = tbody.append("td");
+            var cell = v.append("td");
             cell.text(value);
         });
     })
 }
 
 function boom() {
-
     
     d3.event.preventDefault();
 
  
  var j = d3.select("#datetime").property("value");
-
- 
  
  console.log(j);
  
  
  if (j) {
  var k = data.filter(dates => dates.datetime === j); }
-
     
     console.log(k);
-
     
     tabble(k);
 };
 
+var t = d3.select("#filter-btn");
 
-var button = d3.select("#filter-btn");
-
-var form = d3.select("#form");
+var u = d3.select("#form");
 
 
-button.on("click", boom);
-form.on("submit", boom);
+t.on("click", boom);
+u.on("submit", boom);
 
 
